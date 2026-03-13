5487cabad501f18664709400e7a757b9cb6663db
Flarkinater <mcteaguealex@gmail.com>
2026-03-13 18:07:46 -0400

    Add basic main scene and script

---
+++ VoltorbFlipSolver.cs
@@ -0,0 +1,12 @@
+using Godot;
+
+namespace VoltorbFlipSolver;
+
+public partial class VoltorbFlipSolver : Node2D {
+	[Export] public GridContainer board;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready() {
+		
+	}
+}
